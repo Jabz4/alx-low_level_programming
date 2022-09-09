6cf82eba65355964c2c4fd6aeb1b6e2eca77ac3e
jabz4 <jabir.omarr@gmail.com>
2022-09-09 00:15:57 -0700

    nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,19 @@
 /**
  * main - print the string 
  *
- * Return (0)
+ * Return: (0)
  */
+
 int main(void)
 {
 	int g;
-	long int c;
-	long long int d;
+	long c;
+	long long d;
 	char h;
 	float k;
 	printf("size of a int: %ld byte(s)\n", sizeof(g));
- 	printf("size of a long int: %ld byte(s)\n", sizeof(c));
-        printf("size of a long long int: %ld byte(s)\n", sizeof(d)); 
+ 	printf("size of a long: %ld byte(s)\n", sizeof(c));
+        printf("size of a long long: %ld byte(s)\n", sizeof(d)); 
         printf("size of a char: %ld byte(s)\n", sizeof(h));        
         printf("size of a float: %ld byte(s)\n", sizeof(k)); 
         return (0);
