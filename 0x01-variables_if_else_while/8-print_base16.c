355e34df34ffd978f06ae4079ac182f54f85ff71
jabz4 <jabir.omarr@gmail.com>
2022-09-11 07:09:33 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+/**
+ * main - print main function
+ *
+ * description: using main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char j;
+	for (j ='0'; j<='9'; j++)
+	{
+		putchar(j);
+	}
+	for (j ='a'; j<='f'; j++)
+	
+	{ 
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
