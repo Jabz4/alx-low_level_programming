12625574be3c6921e02d59d7244f7b462b612385
jabz4 <jabir.omarr@gmail.com>
2022-09-11 07:16:12 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *
+ * main - print main function
+ *
+ * description: usage of main function 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = 48; g <=57; g++)
+	{
+		putchar(g);
+		if (g != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
