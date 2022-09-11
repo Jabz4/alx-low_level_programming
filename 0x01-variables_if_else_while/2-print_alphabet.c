cb78b18b2ff96bf17cf0c9ae3b2df6f4003c46de
jabz4 <jabir.omarr@gmail.com>
2022-09-11 02:28:37 -0700

    hgdf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - print main function
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char h;
+	for (h = 'a' ; h <= 'z' ; h++)
+	{
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}
