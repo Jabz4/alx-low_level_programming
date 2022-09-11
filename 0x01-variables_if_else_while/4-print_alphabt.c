91d2f90f8ddee51a0c4f342d870f990436b4fc2c
jabz4 <jabir.omarr@gmail.com>
2022-09-11 05:22:48 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,47 @@
+#include <stdio.h>                                                                                                                                       
+
+/**                                                                                                                                                      
+ *
+ *  *                                                                                                                                                       
+ *
+ *   * main - print main function                                                                                                                            
+ *
+ *    *                                                                                                                                                       
+ *
+ *     * description: using main function                                                                                                                      
+ *
+ *      *                                                                                                                                                       
+ *
+ *       * Return: 0                                                                                                                                             
+ *
+ *        */                                                                                                                                                      
+
+int main(void)                                                                                                                                           
+
+	                                                                                                                                                         
+
+{                                                                                                                                                        
+
+	        char h;                                                                                                                                          
+
+		        for (h = 'a' ; h <= 'z' ; h++)                                                                                                                   
+
+				        {                                                                                                                                                
+
+						                putchar(h);                                                                                                                              
+
+								        }                                                                                                                                                
+
+			        for (h != 'e' && h != 'q')                                                                                                                   
+
+					        {                                                                                                                                                
+
+							                putchar(h);                                                                                                                              
+
+									        }                                                                                                                                                
+
+				        putchar('\n');                                                                                                                                   
+
+					        return (0);                                                                                                                                      
+
+}      
