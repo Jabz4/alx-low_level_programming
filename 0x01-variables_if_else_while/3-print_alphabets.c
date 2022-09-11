21d298fc471d79f27d9e036a2581d52dfdfc320c
jabz4 <jabir.omarr@gmail.com>
2022-09-11 05:07:34 -0700

    alpabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *
+ * main - print main function
+ *
+ * description: using main function
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	char h;
+	for (h = 'a' ; h <= 'z' ; h++)
+	{
+		putchar(h);
+	}
+	for (h = 'A' ; h <= 'Z' ; h++)
+	{
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}
