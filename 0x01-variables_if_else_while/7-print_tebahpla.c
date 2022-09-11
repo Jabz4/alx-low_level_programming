3ba12c4686b271ce1d06cf37655c74aaccad9389
jabz4 <jabir.omarr@gmail.com>
2022-09-11 07:02:01 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -print maint function
+ *
+ * description: using main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char p;
+	for (p ='z'; p>='a'; p--)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
