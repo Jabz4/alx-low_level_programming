1e5fc78f868d79e428fcfd9666e0ff406e95785a
jabz4 <jabir.omarr@gmail.com>
2022-09-19 04:38:56 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * void prototypes()
+ * int prototypes()
+ *
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int_strlen(char *s);
@@ -8,3 +16,4 @@ void puts_half(char *str);
 void print_array(int *a, int *n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
+#endif /*MAIN_H*/
