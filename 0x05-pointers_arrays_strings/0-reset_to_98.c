2fec27b367910a429d79fe232e7ee06b74c6d2aa
jabz4 <jabir.omarr@gmail.com>
2022-09-20 02:35:58 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,11 @@
 
 /**
  *
- * *reset_to_98 - resets the value of given integer to 98
+ *reset_to_98 - resets the value of given integer to 98
  *
- * *@n: parameter
+ *@n: parameter
  *
- * *Return : nothing
+ *Return : nothing
  *
  */
 
