0f026b18d537cb6e11eff60ea99594773aa98fcb
jabz4 <jabir.omarr@gmail.com>
2022-09-29 06:53:32 -0700

    rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
-#include <main.h>
-/**
+#include "main.h"                                                                                                                       
+
+                                                                                                                                        
+
+/**                                                                                                                                     
  *
- *  * _puts_recursion - Prints a string followed by a new line
+ *  * _puts_recursion - Prints a string, followed by a new line.                                                                           
  *
- *   * @s: string
+ *   * @s: The string to be printed.                                                                                                        
  *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+ *    */                                                                                                                                     
 
-void _puts_recursion(char *s)
+void _puts_recursion(char *s)                                                                                                           
 
-{
+{                                                                                                                                       
 
-		if (*s == 0)
+	        if (*s)                                                                                                                         
 
-				{
+			        {                                                                                                                               
 
-							_putchar('\n');
+					                _putchar(*s);                                                                                                           
 
-									return;
+							                _puts_recursion(s + 1);                                                                                                 
 
-										}
+									        }                                                                                                                               
 
-			_putchar(*s);
+		                                                                                                                                        
 
-				_puts_recursion(s + 1);
+		        else                                                                                                                            
+
+				                _putchar('\n');                                                                                                         
 
 }
+
+
