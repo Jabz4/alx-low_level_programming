be9eaa6ee44b62db60050f93963f284931827a2d
jabz4 <jabir.omarr@gmail.com>
2022-10-06 04:40:01 -0700

    s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,15 +11,14 @@
  *
  *      * On error, -1 is returned, and errno is set appropriately.
  *
- *       */
+ **/
 
 void _puts_recursion(char *s)
 
 {
 
 		if (*s == 0)
-
-				{
+	{
 
 							_putchar('\n');
 
