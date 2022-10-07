d9bad79f8205c7231efee897bf4b5bf470622d1f
jabz4 <jabir.omarr@gmail.com>
2022-10-07 01:10:21 -0700

    h

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,11 +11,11 @@
  *
  *      * On error, -1 is returned, and errno is set appropriately.
  *
- * */
+ **/
 
 void _print_rev_recursion(char *s)
 
-{	
+{
 	if (*s != '\0')	
 	{
 	_print_rev_recursion(s + 1);
